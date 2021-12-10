7e2ca2ec056e4364f9d30b635bde99547f6d5bf1
SleepySchedule <charleszhao0@gmail.com>
2021-12-10 11:09:11 -0500

    Update snowball, snowman, and play_view

--- christmas/play_view.py
+++ christmas/play_view.py
@@ -1,56 +1,70 @@
 from typing import List
 
-import pygame, random
+import pygame, math
 from pygame.constants import KEYDOWN, MOUSEBUTTONDOWN, K_p
 
 from christmas.base_view import BaseView
 from christmas.pause_view import PauseView
 from christmas.game import Game
-from christmas.sprites import Snowman, Target, Snowball
+from christmas.sprites import Snowman, Snowball
 from christmas.end_view import EndView
 
 class PlayView(BaseView):
     def __init__(self) -> None:
         self.font = pygame.font.SysFont("Arial", 30)
 
-        snowman_pos = pygame.Vector2((1000, 500))
-        self.player_1 = Snowman(snowman_pos)
-        self.snowballs: List[Snowball]=[]
-        self.targets: List[Target]=[]
+        self.player_1_loc = (1000, 500)
+        self.player_1 = Snowman(self.player_1_loc)
+        self.existing_snowball = False
+        # self.snowballs: List[Snowball]=[]
+        # self.targets: List[Target]=[]
 
-        for _ in range(10):
-            pos = pygame.Vector2((random.randint(100,1180), 220))
-            speed = pygame.Vector2((3,0))
-            num = random.randint(0, 1)
-            if num:
-                direction = "right"
-            else:
-                direction = "left"
-            t = Target(pos, speed, direction)
+        # for _ in range(10):
+        #     pos = pygame.Vector2((random.randint(100,1180), 220))
+        #     speed = pygame.Vector2((3,0))
+        #     num = random.randint(0, 1)
+        #     if num:
+        #         direction = "right"
+        #     else:
+        #         direction = "left"
+        #     t = Target(pos, speed, direction)
 
-            self.targets.append(t)
+        #     self.targets.append(t)
 
 
     def event_loop(self, events: List[pygame.event.Event]) -> None:
         for event in events:
             if event.type == MOUSEBUTTONDOWN:
-                mouse_loc = event.pos
-                mouse_loc = pygame.Vector2(mouse_loc)
-                self.snowballs.append(self.player_1.throw_snowball(mouse_loc))
+                # mouse_loc = event.pos
+                # mouse_loc = pygame.Vector2(mouse_loc)
+                # self.snowballs.append(self.player_1.throw_snowball(mouse_loc))
+                if not self.existing_snowball:
+                    self.existing_snowball = True
+                    self.snowball = Snowball()
+                    distance_x = event.pos[0] - self.player_1_loc[0]
+                    distance_y = event.pos[1] - self.player_1_loc[1]
+                    angle = math.atan(distance_y/distance_x)
+                    distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
+                    self.snowball.set_velocity(distance)
+                    self.snowball.set_angle(angle)
             elif event.type == KEYDOWN:
                 if event.key == K_p:
                     Game.set_current_view(PauseView(self))
 
 
     def update(self) -> None:
+        if self.existing_snowball:
+            if self.snowball.is_dead():
+                self.existing_snowball = False
+            else:
+                self.player_1.update_snowball(self.snowball)
         # keys_pressed = pygame.key.get_pressed()
         # self.snowman.update(keys_pressed)
+        # for snowball in self.snowballs:
+        #     snowball.update()
 
-        for snowball in self.snowballs:
-            snowball.update()
-
-        for target in self.targets:
-            target.update()
+        # for target in self.targets:
+        #     target.update()
 
 
     def draw(self, surface: pygame.Surface) -> None:
@@ -58,9 +72,12 @@ class PlayView(BaseView):
 
         self.player_1.draw(surface)
 
-        for snowball in self.snowballs:
-            snowball.draw(surface)
+        if self.existing_snowball:
+            self.snowball.draw(surface)
 
-        for target in self.targets:
-            target.draw(surface)
+        # for snowball in self.snowballs:
+        #     snowball.draw(surface)
+
+        # for target in self.targets:
+        #     target.draw(surface)
 
--- christmas/sprites.py
+++ christmas/sprites.py
@@ -6,11 +6,7 @@ import math, random
 from pygame.constants import K_a, K_d, K_s, K_w
 
 
-class Sprite:
-    def __init__(self, pos: pygame.Vector2, speed: Optional[pygame.Vector2]=None):
-        self._pos = pos
-        self._speed = speed
-
+class Sprite(ABC):
     @abstractmethod
     def draw(self) -> None: ...
 
@@ -19,49 +15,22 @@ class Sprite:
 
 
 class Snowman(Sprite):
+    def __init__(self, pos: Tuple[int, int]):
+        self.snowman = pygame.image.load("snowman.png")
+        self.snowman = pygame.transform.scale(self.snowman, (100, 100))
+        self._pos = pos
+
     def update(self):
         pass
 
     def draw(self, surface: pygame.Surface):
-        # body
-        pygame.draw.circle(surface, (255,255,255), (self._pos), 12)
-        pygame.draw.circle(surface, (255,255,255), (self._pos[0],self._pos[1]+25), 20)
-        pygame.draw.circle(surface, (255,255,255), (self._pos[0],self._pos[1]+55), 28)
-        
-        # arms
-        pygame.draw.line(surface, (150,75,0), (self._pos[0]-10,self._pos[1]+15), (self._pos[0]-40,self._pos[1]+40), 5)
-        pygame.draw.line(surface, (150,75,0), (self._pos[0]+10,self._pos[1]+15), (self._pos[0]+40,self._pos[1]+40), 5)
-        
-        # eyes
-        pygame.draw.circle(surface, (0,0,0), (self._pos[0]-5,self._pos[1]-2), 3)
-        pygame.draw.circle(surface, (0,0,0), (self._pos[0]+5,self._pos[1]-2), 3)
+        surface.blit(self.snowman, self._pos)
 
-        # mouth
-        pygame.draw.arc(surface, (0,0,0), (self._pos[0]-6,self._pos[1]+1, 12, 4), math.pi, math.pi*2, 3)
-
-        # buttons
-        pygame.draw.circle(surface, (0,0,0), (self._pos[0],self._pos[1]+25), 3)
-        pygame.draw.circle(surface, (0,0,0), (self._pos[0],self._pos[1]+40), 3)
-        pygame.draw.circle(surface, (0,0,0), (self._pos[0],self._pos[1]+55), 3)
-
-        # hat
-        pygame.draw.line(surface, (0,0,0), (self._pos[0]-15,self._pos[1]-11), (self._pos[0]+15,self._pos[1]-11), 2)
-        pygame.draw.rect(surface, (0,0,0), (self._pos[0]-10,self._pos[1]-20, 20, 10))
-
-    def throw_snowball(self, end_pos: pygame.Vector2):
-        # start_pos = pygame.Vector2((self.x + 40, self.y + 40))
-        # end_pos = pygame.Vector2(end_pos)
-        start_pos = pygame.Vector2(self._pos)
-        speed = end_pos - start_pos
-        speed.normalize_ip()
-        speed *= 5
-        snowball_pos = pygame.Vector2(start_pos)
-        snowball = Snowball(snowball_pos, speed)
-        # snowball.update(end_pos)
-        return snowball
-
-    def set_pos(self, pos: Tuple[int, int]):
-        self._pos = pos
+    def update_snowball(self, snowball: 'Snowball'):
+        snowball.update(self)
+   
+    def get_pos(self):
+        return self._pos
    
     def get_hitbox(self):
         pass
@@ -69,23 +38,38 @@ class Snowman(Sprite):
 
 class Snowball(Sprite):
     def __init__(self) -> None:
-        self.x0 = 0
-        self.y0 = 400
         self.gravity = 9.81
-        self.angle = 70
-        self.velocity = 80
+        self._angle = 0
+        self._velocity = 0
+
         self.x = 0
         self.y = 0
-
-        vx = self.velocity * math.cos(math.radians(self.angle))
-        vy = self.velocity * math.sin(math.radians(self.angle))
         self.t = 0
 
-    def update(self) -> None:
+    def update(self, snowman: Snowman) -> None:
         self.t += 0.2
-        self.x = self.x0 + self.vx*self.t
-        self.y = self.y0 - (self.vy*self.t - 0.5*self.gravity*self.t**2)
-            
+
+        self.x0, self.y0 = snowman.get_pos()
+
+        vx = -(self._velocity * math.cos(self._angle))
+        vy = self._velocity * math.sin(self._angle)
+
+        self.x = self.x0 + vx*self.t
+        self.y = self.y0 - (vy*self.t - 0.5*self.gravity*self.t**2)
+
+    def is_dead(self) -> bool:
+        if self.y > 720:
+            dead = True
+        else:
+            dead = False
+        return dead
+
     def draw(self, surface: pygame.Surface) -> None:
-        surface.fill((255, 255, 255))
+        
         pygame.draw.circle(surface, (0, 0, 255), (self.x, self.y), 30)
+
+    def set_velocity(self, distance: float):
+        self._velocity = distance//5
+
+    def set_angle(self, angle: int):
+        self._angle = angle
